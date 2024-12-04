c08a0accf3f5f58a2a90b0b8b0eaf06db5b2b961
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-12-04 13:19:47 +0500

    added tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
